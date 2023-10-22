810c2f0b91b2808da1341d9b1e3e1200df498873
Geetansh <geetanshrajesh@gmail.com>
2023-10-22 11:24:52 +0530

    Initial Commit

---
+++ WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by Geetansh Rajesh on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
