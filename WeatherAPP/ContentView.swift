1240c602f84d6300912242a99a7898279a292e40
GeetanshRajesh <geetanshrajesh@gmail.com>
2025-04-07 20:07:46 +0530

    componentalization

--- WeatherAPP/ContentView.swift
+++ WeatherAPP/ContentView.swift
@@ -16,42 +16,46 @@ struct ContentView: View {
                     .padding(.bottom,40)
                 HourlyForcastView(viewModel: HourlyForcastView.ViewModel.mock)
                 DailyForecastView()
+
             }
             .padding(.horizontal)
         }
         .background(.blue)
+        
+    }
+        var uvIndex : some View{
+            VStack{
+                HStack{
+                    Image(systemName: "sun.max.fill")
+                    Text("UV Index".uppercased())
+                }
+                Text("0")
+                Text("Low")
+                ProgressView(value: 0)
+                progressViewStyle(RangedProgressView(range: 0...1.0, foregroundColor: AnyShapeStyle(.blue), backgroundColor:.gray))
+                Text("Use sun protection from 9AM-3PM")
+            }
+            .padding()
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
+        }
+        
+        var sunSet : some View{
+            VStack{
+                HStack{
+                    Image(systemName: "sun.max.fill")
+                    Text("Sunset".uppercased())
+                }
+                Text("6:45PM")
+                ProgressView(value: 0)
+                progressViewStyle(RangedProgressView(range: 0...1.0, foregroundColor: AnyShapeStyle(.blue), backgroundColor:.gray))
+                Text("Sunrise: 7:38AM")
+            }
+            .padding()
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
+        }
+        
     }
     
-    var uvIndex : some View{
-        VStack{
-            HStack{
-                Image(systemName: "sun.max.fill")
-                Text("UV Index".uppercased())
-            }
-            Text("0")
-            Text("Low")
-//            ProgressView(value: 0)
-//            progressViewStyle(RangedProgressView(range: 0...1.0, foregroundColor: AnyShapeStyle(.blue), backgroundColor:.gray))
-            Text("Use sun protection from 9AM-3PM")
-        }
-        .padding()
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
-    }
-    var sunSet : some View{
-        VStack{
-            HStack{
-                Image(systemName: "sun.max.fill")
-                Text("Sunset".uppercased())
-            }
-            Text("6:45PM")
-            ProgressView(value: 0)
-            progressViewStyle(RangedProgressView(range: 0...1.0, foregroundColor: AnyShapeStyle(.blue), backgroundColor:.gray))
-            Text("Sunrise: 7:38AM")
-        }
-        .padding()
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
-    }
-
     var WeatherSummaryViewModel:WeatherSummaryView.ViewModel{
         return WeatherSummaryView.ViewModel.SunnymockData
     }
@@ -60,4 +64,4 @@ struct ContentView: View {
             ContentView()
         }
     }
-}
+
